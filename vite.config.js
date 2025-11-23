1235a3c05c913e42e9f2e90d11a921fc04d2a23e
Budi Irwan Firmansyah <maika.kanaka@gmail.com>
2025-11-24 00:25:26 +0700

    route / for production

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Sesuaikan path dasar untuk GitHub Pages
+const repoName = '/migunesia-queue-fe/' // Ganti dengan nama repositori Anda!
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? repoName : '/',
   plugins: [react()],
 })
