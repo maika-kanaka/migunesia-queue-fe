938903cdd391dd061b2b44ea2934c7a2f1afb64c
Muhamad Jukahpin <muhamadjukahpin@gmail.com>
2025-11-25 23:14:02 +0700

    menggunakan hashrouter rekomendasi dari github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import App from "./App.jsx";
 import theme from "./theme.js";
@@ -9,9 +9,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter basename="/migunesia-queue-fe">
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
