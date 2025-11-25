b61ecf027ef2888147e3badba02bb2696786353e
Muhamad Jukahpin <muhamadjukahpin@gmail.com>
2025-11-25 10:20:52 +0700

    Vite api pindah ke .env.production

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // Sesuaikan path dasar untuk GitHub Pages
-const repoName = '/migunesia-queue-fe/' // Ganti dengan nama repositori Anda!
+const repoName = "/migunesia-queue-fe/"; // Ganti dengan nama repositori Anda!
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: repoName,
   plugins: [react()],
-})
+});
